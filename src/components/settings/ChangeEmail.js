56fd0927021c9e8d5cc2ce7259ac52fc3755c806
Samuele Dassatti <samuele@igloo.ooo>
2018-10-31 17:09:09 +0100

    Toggle icon polyfill for Edge

--- src/components/settings/ChangeEmail.js
+++ src/components/settings/ChangeEmail.js
@@ -148,11 +148,21 @@ class ChangeMailDialog extends React.Component {
                           }
                           tabIndex="-1"
                         >
-                          <ToggleIcon
-                            on={this.state.showPassword || false}
-                            onIcon={<Icon>visibility_off</Icon>}
-                            offIcon={<Icon>visibility</Icon>}
-                          />
+                          {/* fix for ToggleIcon glitch on Edge */}
+                          {document.documentMode ||
+                              /Edge/.test(navigator.userAgent) ? (
+                                this.state.showPassword ? (
+                                  <Icon>visibility_off</Icon>
+                                ) : (
+                                  <Icon>visibility</Icon>
+                                )
+                              ) : (
+                                <ToggleIcon
+                                  on={this.state.showPassword || false}
+                                  onIcon={<Icon>visibility_off</Icon>}
+                                  offIcon={<Icon>visibility</Icon>}
+                                />
+                              )}
                         </IconButton>
                       </InputAdornment>
                     ) : null
