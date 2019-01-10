d50389024f596743f26322240463c6e0981a99aa
Zanin Andrea <andrea@igloo.ooo>
2019-01-10 21:06:09 +0100

    handle new notification types

--- public/webPushSw.js
+++ public/webPushSw.js
@@ -1,6 +1,32 @@
 self.addEventListener("push", function(event) {
   const pushData = JSON.parse(event.data.text())
-  const title = pushData.device.name
+  let title
+
+  switch (pushData.type) {
+    case "DEVICE_NOTIFICATION":
+      title = pushData.device.name
+      break
+
+    case "SHARE_RECEIVED_NOTIFICATION":
+      title = "Environment shared with you"
+      break
+
+    case "SHARE_ACCEPTED_NOTIFICATION":
+      title = "You share was accepted"
+      break
+
+    case "CHANGE_OWNER_RECEIVED_NOTIFICATION":
+      title = "Ownership transfer requested"
+      break
+
+    case "OWNER_CHANGE_ACCEPTED_NOTIFICATION":
+      title = "Your ownership transfer was accepted"
+      break
+
+    default:
+      break
+  }
+
   const options = {
     body: pushData.content,
   }
